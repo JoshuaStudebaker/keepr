56b4c82bee777be2e3258ac9c36c0b354a7890da
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-23 16:56:17 -0600

    updated keep table

--- Setup.sql
+++ Setup.sql
@@ -9,16 +9,15 @@
 
 -- SELECT * FROM profiles
 
-CREATE TABLE keeps(
-  id int AUTO_INCREMENT,
-  creatorId VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  description VARCHAR(255) DEFAULT "No description available",
-  img VARCHAR(255) DEFAULT "https://images.unsplash.com/photo-1470104240373-bc1812eddc9f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  views int DEFAULT 0,
-  shares int DEFAULT 0,
-  keeps int DEFAULT 0,
-
+-- CREATE TABLE keeps(
+--   id int AUTO_INCREMENT,
+--   creatorId VARCHAR(255) NOT NULL,
+--   name VARCHAR(255) NOT NULL,
+--   description VARCHAR(255) DEFAULT "No description available",
+--   img VARCHAR(255) DEFAULT "https://images.unsplash.com/photo-1470104240373-bc1812eddc9f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+--   views int DEFAULT 0,
+--   shares int DEFAULT 0,
+--   keeps int DEFAULT 0,
   
 --   PRIMARY KEY (id),
 --   FOREIGN KEY (creatorId)
@@ -37,6 +36,11 @@ CREATE TABLE keeps(
 -- ("8700cd9b-3848-4114-9936-c047349efa80", "Second Ever Keep","https://images.unsplash.com/photo-1470104240373-bc1812eddc9f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" )
 
 -- SELECT * FROM keeps
+
+-- INSERT INTO keeps
+-- (creatorId, name)
+-- VALUES
+-- ("8700cd9b-3848-4114-9936-c047349efa80", "Third Ever Keep" )
 
 
 
