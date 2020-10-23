0a4535e9942a9a3bcc417937787f791740ff9848
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-23 16:03:29 -0600

    keep model

---
+++ Models/Keep.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Keepr.Models
+{
+  public class Keep
+  {
+    public int Id { get; set; }
+    public string CreatorId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Img { get; set; }
+    public int Views { get; set; }
+    public int Shares { get; set; }
+    public int Keeps { get; set; }
+    public Profile Creator { get; set; }
+  }
+
+  // REVIEW Come back, likely, and make the VaultKeepViewModel
+
+
+}
